99efc7e3608fcbccc6bcf4ad1acb8d26ed03a950
tidr501 <tidr501@PC30491>
2024-09-23 15:18:10 +0200

    k

--- Gruppen!!!/Program.cs
+++ Gruppen!!!/Program.cs
@@ -9,9 +9,6 @@
         static void Main(string[] args)
         {
         }
-            static int Multiply(int a, int b)
-            {
-                return a * b;
-            }
+            
     }
 }
