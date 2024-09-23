946bdc337be8a215bd786b49d9b2b5ec93cbd954
shobah24 <shokran.bahram14@gmail.se>
2024-09-23 15:37:51 +0200

    Multi

--- Gruppen!!!/Program.cs
+++ Gruppen!!!/Program.cs
@@ -2,9 +2,9 @@
 {
     internal class Program
     {
-        static int Addition(int a, int b)
+        static int Multiply(int a, int b)
         {
-            return a + b;
+            return a * b;
         }
         static void Main(string[] args)
         {
